509f4ddfd9c3c4f0636e5abab5afdf3e230a1106
Abbondanzo <peter@abbondanzo.com>
2019-01-18 22:46:46 -0500

    Glowing

--- Assets/Scripts/GazeManager.cs
+++ Assets/Scripts/GazeManager.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Camera))]
 public class GazeManager : MonoBehaviour
 {
     private Camera cam;
+    private GlowObject currentObject;
 
     // Start is called before the first frame update
     void Start()
@@ -15,9 +17,25 @@ public class GazeManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (this.currentObject != null)
+        {
+            this.currentObject.OnRaycastEnd();
+        }
         if (Physics.Raycast(this.cam.transform.position, this.cam.transform.forward, out RaycastHit hitInfo, 20.0f, Physics.DefaultRaycastLayers))
         {
-            Debug.Log(hitInfo);
+            GlowObject glowObject = hitInfo.transform.gameObject.GetComponent<GlowObject>();
+            if (glowObject != null)
+            {
+                glowObject.OnRaycastHit();
+                this.currentObject = glowObject;
+            }
         }
+
+        int speed = 5;
+        if (Input.GetKey(KeyCode.UpArrow)) transform.Translate(new Vector3(0, 1, 0) * Time.deltaTime * speed);
+        if (Input.GetKey(KeyCode.DownArrow)) transform.Translate(new Vector3(0, -1, 0) * Time.deltaTime * speed);
+        if (Input.GetKey(KeyCode.LeftArrow)) transform.Translate(new Vector3(-1, 0, 0) * Time.deltaTime * speed);
+        if (Input.GetKey(KeyCode.RightArrow)) transform.Translate(new Vector3(1, 0, 0) * Time.deltaTime * speed);
+
     }
 }
