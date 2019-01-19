08d473d89b2e418c24a0976344e9a9c44b8cfcf7
Abbondanzo <peter@abbondanzo.com>
2019-01-19 10:23:58 -0500

    Clock countdown

---
+++ Assets/Scripts/ClockAnimator.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class ClockAnimator : MonoBehaviour
+{
+    [SerializeField]
+    protected float duration = 0f;
+
+    [SerializeField]
+    protected Sprite[] frames = null;
+
+    private bool isAnimating;
+    private int currentFrame;
+    private float timeElapsed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetAnimation();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetElapsedTime(float time)
+    {
+        if (timeElapsed == 0f)
+        {
+            GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 0.8f);
+        }
+        timeElapsed = time;
+        CheckFrame();
+    }
+
+    private void ResetAnimation()
+    {
+        isAnimating = false;
+        currentFrame = 0;
+        timeElapsed = 0f;
+        GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 0f);
+    }
+
+    private void CheckFrame()
+    {
+        float timePerFrame = duration / frames.Length;
+        currentFrame = (int)System.Math.Ceiling(timeElapsed / timePerFrame);
+        if (currentFrame < frames.Length && timeElapsed != 0f)
+        {
+            GetComponent<SpriteRenderer>().sprite = frames[currentFrame];
+        }
+        else
+        {
+            ResetAnimation();
+        }
+    }
+}
--- Assets/Scripts/GazeManager.cs
+++ Assets/Scripts/GazeManager.cs
@@ -5,31 +5,27 @@ using UnityEngine;
 [RequireComponent(typeof(Camera))]
 public class GazeManager : MonoBehaviour
 {
+    [SerializeField]
+    protected SpriteRenderer clock;
+
     private Camera cam;
     private GlowObject currentObject;
+    private float timeLookingAtCurrentObject;
+
+    private ClockAnimator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        this.cam = GetComponent<Camera>();
+        cam = GetComponent<Camera>();
+        timeLookingAtCurrentObject = 0f;
+        animator = clock.GetComponent<ClockAnimator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (this.currentObject != null)
-        {
-            this.currentObject.OnRaycastEnd();
-        }
-        if (Physics.Raycast(this.cam.transform.position, this.cam.transform.forward, out RaycastHit hitInfo, 20.0f, Physics.DefaultRaycastLayers))
-        {
-            GlowObject glowObject = hitInfo.transform.gameObject.GetComponent<GlowObject>();
-            if (glowObject != null)
-            {
-                glowObject.OnRaycastHit();
-                this.currentObject = glowObject;
-            }
-        }
+        DetectGlowObjects();
 
         int speed = 5;
         if (Input.GetKey(KeyCode.UpArrow)) transform.Rotate(new Vector3(0, -1, 0) * Time.deltaTime * speed);
@@ -37,5 +33,29 @@ public class GazeManager : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftArrow)) transform.Translate(new Vector3(-1, 0, 0) * Time.deltaTime * speed);
         if (Input.GetKey(KeyCode.RightArrow)) transform.Translate(new Vector3(1, 0, 0) * Time.deltaTime * speed);
 
+    }
+
+    private void DetectGlowObjects()
+    {
+        bool detectedGlowObject = false;
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out RaycastHit hitInfo, 20.0f, Physics.DefaultRaycastLayers))
+        {
+            GlowObject glowObject = hitInfo.transform.gameObject.GetComponent<GlowObject>();
+            if (glowObject != null)
+            {
+                glowObject.OnRaycastHit();
+                currentObject = glowObject;
+                timeLookingAtCurrentObject += Time.deltaTime;
+                animator.SetElapsedTime(timeLookingAtCurrentObject);
+                detectedGlowObject = true;
+            }
+        }
+        if (!detectedGlowObject && currentObject != null)
+        {
+            currentObject.OnRaycastEnd();
+            timeLookingAtCurrentObject = 0f;
+            currentObject = null;
+            animator.SetElapsedTime(timeLookingAtCurrentObject);
+        }
     }
 }
