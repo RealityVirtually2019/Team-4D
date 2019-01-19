00664ced7fa173e459251e0a65a358d20f35765d
Abbondanzo <peter@abbondanzo.com>
2019-01-18 21:40:35 -0500

    Move assets to correct folder

---
+++ Assets/Scripts/GazeManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GazeManager : MonoBehaviour
+{
+    private Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast(this.cam.transform.position, this.cam.transform.forward, out RaycastHit hitInfo, 20.0f, Physics.DefaultRaycastLayers))
+        {
+            Debug.Log(hitInfo);
+        }
+    }
+}
