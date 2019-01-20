d9e0ec574c2369af2d20ad0edb68da676f722243
Abbondanzo <peter@abbondanzo.com>
2019-01-19 22:04:17 -0500

    Fixing random generation of crates

--- Assets/Scripts/RandomPosition.cs
+++ Assets/Scripts/RandomPosition.cs
@@ -1,14 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class RandomPosition : MonoBehaviour
 {
+    [SerializeField]
+    public int numberOfCrates = 3;
+
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 currentPos = gameObject.transform.position;
-        gameObject.transform.position = new Vector3(GetRandomPosition(1.5f, 3f), currentPos.y, GetRandomPosition(1.5f, 3f));
+        Transform crate = gameObject.transform.GetChild(0);
+        Vector3 currentPos = crate.position;
+        float LOW = 1.5f;
+        float HIGH = 3f;
+        float newX = GetRandomPosition(LOW, HIGH);
+        float newZ = GetRandomPosition(LOW, HIGH);
+        float distance = GetDistanceFromCenter(newX, newZ);
+        crate.transform.position = new Vector3(newX, currentPos.y, newZ);
+
+        for (int i = 0; i < numberOfCrates - 1; i++)
+        {
+            GameObject copy = Instantiate(crate.gameObject);
+            newX = GetRandomPosition(LOW, HIGH);
+            newZ = GetZFromX(newX, distance);
+            copy.transform.position = new Vector3(newX, currentPos.y, newZ);
+            copy.transform.parent = gameObject.transform;
+        }
     }
 
     // Update is called once per frame
@@ -29,5 +45,15 @@ public class RandomPosition : MonoBehaviour
             lowerArea = -lowerArea;
         }
         return higherArea + lowerArea;
+    }
+
+    float GetDistanceFromCenter(float x, float z)
+    {
+        return Mathf.Sqrt(Mathf.Pow(x, 2f) + Mathf.Pow(z, 2f));
+    }
+
+    float GetZFromX(float x, float distance)
+    {
+        return Mathf.Sqrt(Mathf.Pow(distance, 2f) - Mathf.Pow(x, 2f));
     }
 }
