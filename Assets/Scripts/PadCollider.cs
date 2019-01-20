cc8fe90734412710d63466ee06b449f22b0b5768
Abbondanzo <peter@abbondanzo.com>
2019-01-20 02:55:31 -0500

    Attaching sorta

--- Assets/Scripts/PadCollider.cs
+++ Assets/Scripts/PadCollider.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class PadCollider : MonoBehaviour
 {
+    // If an object is occupying this space
+    private bool bottomLeft;
+    private bool bottomRight;
+    private bool topLeft;
+    private bool topRight;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        bottomLeft = bottomRight = topLeft = topRight = false;
     }
 
     // Update is called once per frame
@@ -15,4 +21,20 @@ public class PadCollider : MonoBehaviour
     {
         
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.name == "Crate" || collision.collider.tag == "Crate")
+        {
+            Transform crate = collision.transform;
+            Vector3 size = GetComponent<Collider>().bounds.size;
+            if (!bottomLeft)
+            {
+                crate.parent = gameObject.transform;
+                crate.position = new Vector3(-size.x / 2, 0, -size.z / 2);
+            }
+        }
+    }
+
+
 }
